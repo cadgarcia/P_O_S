eaa9b47da7ea5887fc00ac8e4fe9b639016a6deb
joao carlos garcia <cadgarcia@gmail.com>
2021-10-13 07:51:03 -0300

    October-12-21

---
+++ 02.POO_C#/Unidade05/Unidade05/TratamentoExcecao/ExemploAsyncAwait.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TratamentoExcecao
+{
+    public class ExemploAsyncAwait
+    {
+        //método síncrono:
+        public void Sincrono() {
+            HttpClient client = new HttpClient();
+            string conteudo = client.GetStringAsync("http://msdn.microsoft.com").GetAwaiter().GetResult();
+
+            Console.WriteLine("Tamanho da página: {0}", conteudo);
+        }
+
+        //método assíncrono:
+        public async void Assincrono() {
+            // a variavel fica aguardando (await) pelo retorno de outro metódo
+            var tamanho = await AcessaWebAssinc();
+            Console.WriteLine("Tamanho da página: {0}", tamanho);
+        }
+
+        //retorno que precisa ser aguardado do tipo int (Taks<int>)
+        public async Task<int> AcessaWebAssinc() {
+
+            HttpClient client = new HttpClient();
+
+            Task<string> retorno = client.GetStringAsync("http://msdn.microsoft.com");
+
+            // com o uso se await o runtime sabe que precisa esperar esta tarefa ser concluída
+            string resultado = await retorno;
+
+            return resultado.Length;
+        }
+    }
+}
+
--- 02.POO_C#/Unidade05/Unidade05/TratamentoExcecao/MInhasExtencoes.cs
+++ 02.POO_C#/Unidade05/Unidade05/TratamentoExcecao/MInhasExtencoes.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 namespace TratamentoExcecao
 {
-    public class MInhasExtencoes
+    public static class MInhasExtencoes
     {
-        public MInhasExtencoes()
-        {
+        public static bool EhUmCPF(this string texto) {
+            return texto.Length == 11;
+        }
+
+        public static bool EhMeuAniversario(DateTime data) {
+            return data.Date == new DateTime(1974, 3, 16).Date; 
         }
     }
 }
